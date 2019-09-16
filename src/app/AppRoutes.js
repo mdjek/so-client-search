99aaaf8b13cd6b63beab52a047a3ceab3359e1e5
Evgeniy.Barankov <mdjek@mail.ru>
2019-09-16 17:24:24 +0300

    edited routes for gh-pages

--- src/app/AppRoutes.js
+++ src/app/AppRoutes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router-dom';
+import { Route, Switch, Redirect } from 'react-router-dom';
 import { Main } from '../containers/Main/index';
 import { Search } from '../containers/Search/index';
 import { Question } from '../containers/Question/index';
@@ -10,6 +10,7 @@ const AppRoutes = () => (
         <Route exact path="/" component={Main} />
         <Route exact path="/search" component={Search} />
         <Route exact path="/question/:id" component={Question} />
+        <Redirect from="/so-client-search" to="/" />
         <Route component={NotFound} />
     </Switch>
 );
