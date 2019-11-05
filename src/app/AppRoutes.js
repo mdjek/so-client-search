610541107843b83c3304fbc50213f8750c0781fa
mdjek <32070769+mdjek@users.noreply.github.com>
2019-11-05 13:48:27 +0300

    Update AppRoutes.js

--- src/app/AppRoutes.js
+++ src/app/AppRoutes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch, Redirect } from 'react-router-dom';
+import { Route, Switch } from 'react-router-dom';
 import { Main } from '../containers/Main/index';
 import { Search } from '../containers/Search/index';
 import { Question } from '../containers/Question/index';
