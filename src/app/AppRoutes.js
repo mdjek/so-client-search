be1b899895f7101aa1a5314d94ff09d7d7572ada
Evgeniy.Barankov <mdjek@mail.ru>
2019-11-06 11:25:56 +0300

    edited routes

--- src/app/AppRoutes.js
+++ src/app/AppRoutes.js
@@ -1,33 +1,21 @@
 import React from 'react';
-import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import { Route, Router, Switch, BrowserRouter } from 'react-router-dom';
 import { Main } from '../containers/Main/index';
 import { Search } from '../containers/Search/index';
 import { Question } from '../containers/Question/index';
 import { NotFound } from '../components/pageTemplates/index';
-import publictUrl from '../publicUrl';
+import AppHistory from '../app/history';
 
-const AppRoutes = () => {
-  if (process.env.NODE_ENV === 'development') {
-    return (
-      <Switch>
-        <Route exact path="/" component={Main} />
-        <Route exact path="/search" component={Search} />
-        <Route exact path="/question/:id" component={Question} />
-        <Route component={NotFound} />
-      </Switch>
-    );
-  }
+const AppRoutes = () => (
+  <Router history={AppHistory}>
+    <Switch>
+      <Route exact path="/" component={Main} />
+      <Route exact path="/search" component={Search} />
+      <Route exact path="/question/:id" component={Question} />
+      <Route component={NotFound} />
+    </Switch>
+  </Router>
+);
 
-  return (
-    <BrowserRouter basename={publictUrl[process.env.NODE_ENV]}>
-      <Switch>
-        <Route exact path="/" component={Main} />
-        <Route exact path="/search" component={Search} />
-        <Route exact path="/question/:id" component={Question} />
-        <Route component={NotFound} />
-      </Switch>
-    </BrowserRouter>
-  );
-};
 
 export default AppRoutes;
--- src/app/history.js
+++ src/app/history.js
@@ -1,3 +1,4 @@
 import { createBrowserHistory } from 'history';
+import publictUrl from '../publicUrl';
 
-export default createBrowserHistory();
+export default createBrowserHistory({ basename: publictUrl[process.env.NODE_ENV] });
